a28060e73465e19ccca0d23450ba976b022d0414
Skygut <v.chub@outlook.de>
2024-01-15 21:40:37 +0100

    fix second part

--- Part2.py
+++ Part2.py
@@ -31,4 +31,19 @@ def draw_koch_curve(order, size=300):
     window.mainloop()
 
 
-draw_koch_curve(2)
+def main():
+    level = None
+
+    while True:
+        value = input("Enter recursion level:\n>>> ")
+        try:
+            level = int(value)
+            break
+        except ValueError:
+            print(f"Invalid value: {value}. The value must be an integer.")
+
+    draw_koch_curve(level)
+
+
+if __name__ == "__main__":
+    main()
